4be1b98cd5290cac650ef0b642a04e0262bc800d
Jutika Patil <jutikapatil123@gmail.com>
2023-02-16 23:16:37 +0530

    navbar static added

---
+++ components/Navbar.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import NavLogo from "../public/vercel.svg";
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <div className="fixed flex w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image src={NavLogo} alt="/" width="125" height="50" />
+        <div>
+          <ul className="hidden md:flex ">
+            <li className="ml-10 text-sm uppercase hover:border-b-2 border-black">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b-2 border-black">
+              <Link href="/#about">About</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b-2 border-black">
+              <Link href="/#skills">Skills</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b-2 border-black">
+              <Link href="/#projects">Projects</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b-2 border-black">
+              <Link href="/resume">Resume</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b-2 border-black">
+              <Link href="/#contact">Contact</Link>
+            </li>
+          </ul>
+          {/* Hamburger Icon */}
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      <div className=" fixed left-0 top-0 w-full h-screen bg-black/70">
+{/* Side Drawer Menu */}
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Link href="/">
+                <Image src={NavLogo} width="87" height="35" alt="/" />
+              </Link>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4">
+                Let&#39;s build something legendary together
+              </p>
+            </div>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/">
+                <li className="py-4 text-sm">Home</li>
+              </Link>
+              <Link href="/#about">
+                <li className="py-4 text-sm">About</li>
+              </Link>
+              <Link href="/#skills">
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href="/#projects">
+                <li className="py-4 text-sm">Projects</li>
+              </Link>
+              <Link href="/resume">
+                <li className="py-4 text-sm">Resume</li>
+              </Link>
+              <Link href="/#contact">
+                <li className="py-4 text-sm">Contact</li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Let&#39;s Connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <a
+                  href="https://www.linkedin.com/in/jutikapatil16/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaLinkedinIn />
+                  </div>
+                </a>
+                <a
+                  href="https://github.com/jutikapatil16"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaGithub />
+                  </div>
+                </a>
+                <Link href="/#contact">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <AiOutlineMail />
+                  </div>
+                </Link>
+                <Link href="/resume">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <BsFillPersonLinesFill />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
